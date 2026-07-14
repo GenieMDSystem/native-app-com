a4af1935e93d85a51866884d6e58fed996d7ce65
GenieMD System <system@geniemd.com>
2026-07-14 19:54:18 +0530

    Update AppDestinations to replace local HTML assets with external URLs for waiting room and scheduling flows

--- WebViewBrowserBridge/app/src/main/java/com/example/webviewbrowserbridge/AppDestinations.kt
+++ WebViewBrowserBridge/app/src/main/java/com/example/webviewbrowserbridge/AppDestinations.kt
@@ -11,13 +11,16 @@ object AppDestinations {
      * "https://dev.geniemd.net/neurofinity/assessment/#/protocol/..."
      */
     const val VISIT_DOCTOR_URL =
-        "file:///android_asset/waiting_room.html"
+        // "file:///android_asset/waiting_room.html"
+        "https://dev.geniemd.net/neurofinity/assessment/#/protocol/1000254/consent?patientLanguageID=1&patientOEMID=100&ignoreLocalStorage=true&dependent=true&fromWebView=android";
 
     /** Schedule Visit Now → Schedule flow WebView */
     const val SCHEDULE_VISIT_URL =
-        "file:///android_asset/schedule.html"
+        // "file:///android_asset/schedule.html"
+        "https://dev.geniemd.net/neurofinity/assessment/#/protocol/1000254/consent?patientLanguageID=1&patientOEMID=100&ignoreLocalStorage=true&dependent=true&fromWebView=android";
 
     /** Schedules List → list WebView */
     const val SCHEDULES_LIST_URL =
-        "file:///android_asset/schedules_list.html"
+        // "file:///android_asset/schedules_list.html"
+        "https://dev.geniemd.net/neurofinity/assessment/#/protocol/1000254/consent?patientLanguageID=1&patientOEMID=100&ignoreLocalStorage=true&dependent=true&fromWebView=android";
 }
