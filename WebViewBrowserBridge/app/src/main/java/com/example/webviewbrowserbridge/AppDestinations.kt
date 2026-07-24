03cb4c74bc4a5ff028bf71434b817356e68191ef
GenieMD System <system@geniemd.com>
2026-07-24 11:45:05 +0530

    Update AppDestinations to include ignoreLocalStorage and ignoreLocationCheck parameters in URL generation for TeleConsultation scheduling

--- WebViewBrowserBridge/app/src/main/java/com/example/webviewbrowserbridge/AppDestinations.kt
+++ WebViewBrowserBridge/app/src/main/java/com/example/webviewbrowserbridge/AppDestinations.kt
@@ -27,7 +27,7 @@ object AppDestinations {
       append(profile.languageId)
       append("&patientOEMID=")
       append(profile.oemID)
-      append("&protocolName=Revamp%20TeleConsultation&dependent=true&fromWebView=android")
+      append("&protocolName=Revamp%20TeleConsultation&dependent=true&fromWebView=android&ignoreLocalStorage=true")
     }
   }
 
@@ -45,7 +45,7 @@ object AppDestinations {
       append(profile.languageId)
       append("&patientOEMID=")
       append(profile.oemID)
-      append("&protocolName=Revamp%20Scheudle%20a%20TeleConsultation&dependent=true&fromWebView=android")
+      append("&protocolName=Revamp%20Scheudle%20a%20TeleConsultation&dependent=true&fromWebView=android&ignoreLocationCheck=true")
     }
   }
 
